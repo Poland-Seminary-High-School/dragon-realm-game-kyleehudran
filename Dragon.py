9874b374cd358141c8bb2af8098d6527e336f832
kyleehudran <KMhudran@student.polandschools.org>
2025-03-27 10:38:25 -0400

    Add files via upload

---
+++ Dragon.py
@@ -0,0 +1,51 @@
+Import Random
+Import Timeout
+
+def displayIntro():
+    print('''you are in a land full of dragons. In front of you,
+'you will see two caves. In one cave, the dragon is friendly 
+'and will share his treasure with you. The other dragon'
+'is greedy and hungry, and will eat you on sight.'''
+          Print()
+
+def chooseCave():
+    cave=''
+    while cave != '1' and cave != '2'
+         print('Which cave will you go into?(1 or 2)
+    Cave = input()
+
+    Return cave
+
+    def checkCave(chosenCave):
+         Print('you apporch the cave...')
+         time.sleep(2)
+         print('It is dark and spooky...')
+         time.sleep(2)
+         print('A large dragon jumps out in front of you! He opens his jaw and ...')
+         Print()
+         time.sleep(2)
+
+         Friendlycave = random.randit(1.2)
+         
+         if chosenCave = str(friendlyCave):
+             Print('gives you his treasure!')
+          else:
+              print('gobbles you down ib one bite!')
+PlayAgain= 'yes'
+while playAgain == 'yes' or playAgain == 'y':
+    displayIntro()
+    caveNumber = chooseCave()
+    checkCave(caveNumber)
+
+
+    Print('Do you want to play again? (yes or no)')
+    playAgain = input()
+              
+            
+    
+
+
+
+
+    
+    
